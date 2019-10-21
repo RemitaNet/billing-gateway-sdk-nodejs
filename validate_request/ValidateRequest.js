416eaab69c52dd5bbd3e8a12b0c88ed8ad23fc15
omoniyi24 <ilesanmi@Systemspecs.com.ng>
2019-10-21 09:43:00 +0100

    WIP

--- validate_request/ValidateRequest.js
+++ validate_request/ValidateRequest.js
@@ -2,14 +2,12 @@ const getCredential = require('../Credentials');
 const getEnvironment = require('../EnvironmentConfig');
 const sha512 = require('js-sha512');
 const getMethod = require('../BaseMethod');
-let request = require('async-request'),
-    response;
 
 
 async function validateRequest(credential, payload) {
     var getBillingEnv = getEnvironment.setBillingEnvironment(credential);
     var url = getBillingEnv.get('VALIDATE_REQUEST_URL');
-    response = getMethod.basePostMethod(credential, payload, url, getBillingEnv).then( value => {
+    var response = getMethod.basePostMethod(credential, payload, url, getBillingEnv).then( value => {
         return value;
     });
     return response;
